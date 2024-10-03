af5fc08a7fd3abe14f68d8b030d623482ed61696
Srujanmp333 <srujanmp@gmail.com>
2024-10-03 17:20:36 -0400

    Update outputs.tf

--- modules/outputs.tf
+++ modules/outputs.tf
@@ -1,5 +1,3 @@
-
-/*
 output "cluster_id" {
   description = "EKS cluster ID"
   value       = module.eks.cluster_id
@@ -16,4 +14,3 @@ output "cluster_name" {
   value       = module.eks.cluster_name
 }
 
-*/
